27a6c382b783ca7aae2ae8e468b0a82f9ee50830
five-forever <1660327787@qq.com>
2023-12-10 16:34:59 +0800

    Site updated: 2023-12-10 16:34:59

--- PoolGame-Web/Ball.js
+++ PoolGame-Web/Ball.js
@@ -1,106 +1,124 @@
-import * as THREE from './libs/three137/three.module.js';
-import * as CANNON from './libs/cannon-es.js';
+/*
+ * @Author: Talos--1660327787@qq.com
+ * @Date: 2023-12-03 20:52:28
+ * @LastEditors: Talos--1660327787@qq.com
+ * @LastEditTime: 2023-12-10 16:02:02
+ * @FilePath: /PoolGame-Web/Ball.js
+ * @Description: 构造桌球Ball类，完成刚体属性和纹理设置，检测睡眠和碰撞落袋
+ * 
+ * Copyright (c) 2023 by five-forever, All Rights Reserved. 
+ */
+
+import * as THREE from './libs/three137/three.module.js' 
+import * as CANNON from './libs/cannon-es.js' 
 
 class Ball{
-    static RADIUS = 0.05715 / 2;
-    static MASS = 0.17;
-    static MATERIAL = new CANNON.Material("ballMaterial");
+    static RADIUS = 0.05715/2 
+    static MASS = 0.17 
+    static MATERIAL = new CANNON.Material("ballMaterial") 
     
-    constructor(game, x, z, id=0) {
-        this.id = id;
-    
-        this.startPosition = new THREE.Vector3(x, Ball.RADIUS, z);
-        this.mesh = this.createMesh(game.scene);
-        
-        this.world = game.world;
-        this.game = game;
-
-        this.rigidBody = this.createBody(x, Ball.RADIUS, z);
-        this.world.addBody(this.rigidBody);
-        this.reset();
-
-        this.name = `ball${id}`;
+    constructor(game, x, z, id) {
+        this.id = id 
+        this.startPosition = new THREE.Vector3(x, Ball.RADIUS, z) 
+        this.mesh = this.createMesh(game.scene) 
+        this.world = game.world 
+        this.game = game 
+        this.createBody(x, Ball.RADIUS, z) 
+        this.reset() 
+        this.name = `ball${id}` 
     }
-
-    get isSleeping(){
-      return this.rigidBody.sleepState == CANNON.Body.SLEEPING;
+    // 更新刚体属性的睡眠状态
+    get isSleeping() {
+      return this.rigidBody.sleepState == CANNON.Body.SLEEPING 
     }
-
-    reset(){
-      this.rigidBody.velocity = new CANNON.Vec3(0);
-      this.rigidBody.angularVelocity = new CANNON.Vec3(0);
-      this.rigidBody.position.copy( this.startPosition );
-      this.world.removeBody(this.rigidBody);
-      this.world.addBody(this.rigidBody);
-      this.mesh.position.copy( this.startPosition );
-      this.mesh.rotation.set(0,0,0);
-      this.mesh.visible = true;
-      this.fallen = false;
+    // 摆球复位
+    reset() {
+      this.rigidBody.velocity = new CANNON.Vec3(0) 
+      this.rigidBody.angularVelocity = new CANNON.Vec3(0) 
+      this.rigidBody.position.copy(this.startPosition) 
+      this.world.removeBody(this.rigidBody) 
+      this.world.addBody(this.rigidBody) 
+      this.mesh.position.copy(this.startPosition) 
+      this.mesh.rotation.set(0,0,0) 
+      this.mesh.visible = true 
+      this.fallen = false 
     }
-
+    // 球进洞更新状态
     onEnterHole() {
-      this.rigidBody.velocity = new CANNON.Vec3(0);
-      this.rigidBody.angularVelocity = new CANNON.Vec3(0);
-      this.world.removeBody(this.rigidBody);
-      this.fallen = true;
-      this.mesh.visible = false;
-      this.game.updateUI({event: 'balldrop', id: this.id } );
+      this.rigidBody.velocity = new CANNON.Vec3(0) 
+      this.rigidBody.angularVelocity = new CANNON.Vec3(0) 
+      this.world.removeBody(this.rigidBody) 
+      this.fallen = true 
+      this.mesh.visible = false 
+      this.game.updateUI({event: 'balldrop', id: this.id}) 
     }
-    
+    // 创建刚体属性
     createBody(x,y,z) {
       const body = new CANNON.Body({
         mass: Ball.MASS, // kg
         position: new CANNON.Vec3(x,y,z), // m
         shape: new CANNON.Sphere(Ball.RADIUS),
         material: Ball.MATERIAL
-      });
-    
-      body.linearDamping = body.angularDamping = 0.5; // Hardcode
-      body.allowSleep = true;
-    
-      // Sleep parameters
-      body.sleepSpeedLimit = 2; // Body will feel sleepy if speed< 1 (speed == norm of velocity)
-      body.sleepTimeLimit = 0.1; // Body falls asleep after 0.1s of sleepiness
-    
-      return body;
+      }) 
+      body.collisionResponse = true 
+      body.linearDamping = 0.5  // 运动时每帧线性速度减缓50%
+      body.angularDamping = 0.5  // 每帧角速度减缓 50%
+      body.allowSleep = true 
+      body.sleepSpeedLimit = 2  // 速度小于2考虑置为睡眠
+      body.sleepTimeLimit = 0.1  // 静止后0.1s进入睡眠
+      // 添加撞击音效
+      const hitSound = new Audio('./assets/audio/pool-ball-hit.wav') 
+      const playHitSound = collision => {
+        const material = collision.body["material"]
+        if(material == null) return // 未初始化完全
+        // 撞击力度
+        const impactStrength = collision.contact.getImpactVelocityAlongNormal() 
+        // 判断撞击对象和音效
+        if(material["name"]=="ballMaterial" && impactStrength>0.35){
+          hitSound.currentTime = 0
+          hitSound.volume = impactStrength*0.5>1 ? 1 :impactStrength*0.5 
+          hitSound.play() 
+        }
+      }
+      this.rigidBody = body 
+      this.world.addBody(this.rigidBody) 
+      this.rigidBody.addEventListener('collide', playHitSound) 
     }
-
-    createMesh (scene) {
-        const geometry = new THREE.SphereGeometry(Ball.RADIUS, 16, 16);
+    // 创建纹理
+    createMesh(scene){
+        const geometry = new THREE.SphereGeometry(Ball.RADIUS, 32, 32) 
         const material = new THREE.MeshStandardMaterial({
             metalness: 0.0,
             roughness: 0.1,
             envMap: scene.environment
-        });
-  
-        if (this.id>0){
-            const textureLoader = new THREE.TextureLoader().setPath('./assets/pool-table/').load(`${this.id}ball.png`, tex => {
-                material.map = tex;
-                material.needsUpdate = true;
-            });
+        }) 
+        // 读取编号对应图片
+        if(this.id>0){
+            const textureLoader = new THREE.TextureLoader().setPath('./assets/pool-table/')
+                                      .load(`${this.id}ball.png`, tex => {
+                material.map = tex 
+                material.needsUpdate = true 
+            }) 
         }
   
-        const mesh = new THREE.Mesh(geometry, material);
+        const mesh = new THREE.Mesh(geometry, material) 
+        mesh.castShadow = true 
+        mesh.receiveShadow = true 
+        scene.add(mesh) 
     
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
-
-        scene.add(mesh);
-    
-        return mesh;
-    };
+        return mesh 
+    } 
  
     update(dt){
-        if (this.fallen) return;
+        if(this.fallen) return 
 
-        this.mesh.position.copy(this.rigidBody.position);
-        this.mesh.quaternion.copy(this.rigidBody.quaternion);
-      
-        // Has the ball fallen into a hole?
-        if (this.rigidBody.position.y < -Ball.RADIUS && !this.fallen) {
-          this.onEnterHole();
+        this.mesh.position.copy(this.rigidBody.position) 
+        this.mesh.quaternion.copy(this.rigidBody.quaternion) 
+        // 判断进球
+        if (this.rigidBody.position.y<-Ball.RADIUS && !this.fallen) {
+          this.onEnterHole() 
         }
     }
 }
 
-export { Ball };
+export { Ball } 
--- PoolGame-Web/GameUI.js
+++ PoolGame-Web/GameUI.js
@@ -1,125 +1,136 @@
-import { StrengthBar } from "./StrengthBar.js";
+/*
+ * @Author: Talos--1660327787@qq.com
+ * @Date: 2023-12-03 20:52:28
+ * @LastEditors: Talos--1660327787@qq.com
+ * @LastEditTime: 2023-12-09 22:42:16
+ * @FilePath: /PoolGame-Web/GameUI.js
+ * @Description: 管理游戏中出现的ui，切换隐藏和显示
+ * 
+ * Copyright (c) 2023 by five-forever, All Rights Reserved. 
+ */
+
+import { StrengthBar } from "./StrengthBar.js" 
 
 class GameUI{
   constructor() {
-    this.strengthBar = new StrengthBar();
+    this.strengthBar = new StrengthBar() 
   }
-
+  // 添加CSS类
   addClass(el, className) {
     if (el.classList) {
-      el.classList.add(className);
+      el.classList.add(className) 
     } else {
-      el.className += ' ' + className;
+      el.className += ' ' + className 
     }
   }
-
+  // 移除CSS类
   removeClass(el, className) {
     if (el.classList) {
-      el.classList.remove(className);
-    } else {
-      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      el.classList.remove(className) 
+    } else { // TODO：有待优化
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ') 
     }
-  };
-
+  } 
+  // 移除hide属性来展示
   show(elmId) {
-    const node = document.getElementById(elmId);
-    this.removeClass(node, 'hide');
+    const node = document.getElementById(elmId) 
+    this.removeClass(node, 'hide') 
   }
-
+  // 添加hide属性隐藏
   hide(elmId) {
-    const node = document.getElementById(elmId);
-    this.addClass(node, 'hide');
+    const node = document.getElementById(elmId) 
+    this.addClass(node, 'hide') 
   }
-
+  // 更新出手计时器
   updateTimer(timerVal) {
-    document.getElementsByClassName('timer')[0].textContent = timerVal;
+    document.getElementsByClassName('timer')[0].textContent = timerVal 
   }
-
+  // 添加游戏日志
   log(str) {
-    const node = document.createElement('li');
-    node.textContent = str;
-    document.getElementsByClassName('gamelog')[0].appendChild(node);
+    const node = document.createElement('li') 
+    node.textContent = str 
+    document.getElementsByClassName('gamelog')[0].appendChild(node) 
   }
-
+  // 更新当前玩家名
   updateTurn(str) {
-    this.removeClass(document.getElementsByClassName('player1')[0], 'active');
-    this.removeClass(document.getElementsByClassName('player2')[0], 'active');
-    this.addClass(document.getElementsByClassName(str)[0], 'active');
+    this.removeClass(document.getElementsByClassName('player1')[0], 'active') 
+    this.removeClass(document.getElementsByClassName('player2')[0], 'active') 
+    this.addClass(document.getElementsByClassName(str)[0], 'active') 
   }
+  // 更新球的状态（solid、striped、unknown） ballArr记录落袋的球
+  updateBalls(ballArr, sides){
+    const p1side = sides.player1 == '?' ? 'unknown' : sides.Player1 
+    const p2side = sides.player2 == '?' ? 'unknown' : sides.Player2 
 
-  updateBalls(ballArr, sides) {
-    const p1side = sides.player1 == '?' ? 'unknown' : sides.player1;
-    const p2side = sides.player2 == '?' ? 'unknown' : sides.player2;
+    this.removeClass(document.getElementsByClassName('player1')[0], 'solid') 
+    this.removeClass(document.getElementsByClassName('player2')[0], 'solid') 
+    this.removeClass(document.getElementsByClassName('player1')[0], 'striped') 
+    this.removeClass(document.getElementsByClassName('player2')[0], 'striped') 
+    this.removeClass(document.getElementsByClassName('player1')[0], 'unknown') 
+    this.removeClass(document.getElementsByClassName('player2')[0], 'unknown') 
+    this.addClass(document.getElementsByClassName('player1')[0], p1side) 
+    this.addClass(document.getElementsByClassName('player2')[0], p2side) 
 
-    this.removeClass(document.getElementsByClassName('player1')[0], 'solid');
-    this.removeClass(document.getElementsByClassName('player2')[0], 'solid');
-    this.removeClass(document.getElementsByClassName('player1')[0], 'striped');
-    this.removeClass(document.getElementsByClassName('player2')[0], 'striped');
-    this.removeClass(document.getElementsByClassName('player1')[0], 'unknown');
-    this.removeClass(document.getElementsByClassName('player2')[0], 'unknown');
-    this.addClass(document.getElementsByClassName('player1')[0], p1side);
-    this.addClass(document.getElementsByClassName('player2')[0], p2side);
+    if(p1side == 'unknown') return 
 
-    if (p1side == 'unknown') return;
+    let elem = document.createElement('ul') 
 
-    let elem = document.createElement('ul');
-
-    for (let i=1;i<8;i++) {
-      const el = document.createElement('li');
-      el.textContent = i;
-      if (ballArr.indexOf(i) > -1) {
+    for (let i=1; i<8; i++){
+      const el = document.createElement('li') 
+      el.textContent = i 
+      if(ballArr.indexOf(i) > -1){
 
       } else {
-        this.addClass(el, 'pocketed');
+        this.addClass(el, 'pocketed') 
       }
 
-      elem.appendChild(el);
+      elem.appendChild(el) 
     }
 
-    document.getElementsByClassName(p1side == 'solid' ? 'player1' : 'player2')[0].replaceChild(elem, document.getElementsByClassName(p1side == 'solid' ? 'player1' : 'player2')[0].children[1]);
+    document.getElementsByClassName(p1side == 'solid' ? 'player1' : 'player2')[0].replaceChild(elem, document.getElementsByClassName(p1side == 'solid' ? 'player1' : 'player2')[0].children[1]) 
     
-    elem = document.createElement('ul');
+    elem = document.createElement('ul') 
 
     for (let i=9; i<16; i++) {
-      const el = document.createElement('li');
-      el.textContent = i;
-      if (ballArr.indexOf(i) > -1) {
+      const el = document.createElement('li') 
+      el.textContent = i 
+      if(ballArr.indexOf(i) > -1){
 
       } else {
-        this.addClass(el, 'pocketed');
+        this.addClass(el, 'pocketed') 
       }
 
-      elem.appendChild(el);
+      elem.appendChild(el) 
     }
 
-    document.getElementsByClassName(p1side == 'striped' ? 'player1' : 'player2')[0].replaceChild(elem, document.getElementsByClassName(p1side == 'striped' ? 'player1' : 'player2')[0].children[1]);
+    document.getElementsByClassName(p1side == 'striped' ? 'player1' : 'player2')[0].replaceChild(elem, document.getElementsByClassName(p1side == 'striped' ? 'player1' : 'player2')[0].children[1]) 
   }
-
+  // 显示或隐藏HUD
   showGameHud(mode){
     if (mode){
-      this.hide('playBtn');
-      this.show('player1');
-      this.show('player2');
-      this.show('timer');
-      this.show('gamelog');
+      this.hide('playBtn') 
+      this.show('player1') 
+      this.show('player2') 
+      this.show('timer') 
+      this.show('gamelog') 
     }else{
-      this.show('playBtn');
-      this.hide('player1');
-      this.hide('player2');
-      this.hide('timer');
-      this.hide('gamelog');
+      this.show('playBtn') 
+      this.hide('player1') 
+      this.hide('player2') 
+      this.hide('timer') 
+      this.hide('gamelog') 
     }
   }
-
+  // 打印消息
   showMessage(title, body) {
-    document.getElementById("message").children[0].textContent = title;
-    document.getElementById("message").children[1].textContent = body;
-    this.show('message');
+    document.getElementById("message").children[0].textContent = title 
+    document.getElementById("message").children[1].textContent = body 
+    this.show('message') 
   }
-
+  // 更新游戏加载进度条
   update(){
-    if (this.strengthBar.visible) this.strengthBar.update();
+    if(this.strengthBar.visible) this.strengthBar.update() 
   }
 }
 
-export { GameUI };
+export { GameUI } 
